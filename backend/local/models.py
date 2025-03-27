a9a9771c484f6c52dd5dd40bf7309f86ee832e10
Juliano <dti.juliano@fimca.com.br>
2025-03-27 13:39:41 -0400

    Create models for 'local' app

--- backend/local/models.py
+++ backend/local/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+TIPO_LOCAL = [
+    ('HOSPITAL', 'Hospital'),
+    ('CLINICA', 'Clínica'),
+]
+
+
+class Local(models.Model):
+    idLocal = models.AutoField(
+        primary_key=True
+    )
+    nome = models.CharField(
+        max_length=255,
+        blank=False,
+        verbose_name='Nome'
+    )
+    cnpj = models.CharField(
+        max_length=14,
+        blank=False,
+        verbose_name='CNPJ'
+    )
+    endereco = models.CharField(
+        max_length=1000,
+        blank=False,
+        verbose_name='Endereço'
+    )
+    tipoLocal = models.CharField(
+        max_length=50,
+        choices=TIPO_LOCAL,
+        blank=False,
+        verbose_name='Tipo Local'
+    )
+    telefone = models.CharField(
+        max_length=11,
+        blank=False,
+        verbose_name='Telefone'
+    )
+
+    def __str__(self):
+        return f"{self.nome} - {self.cnpj}"
