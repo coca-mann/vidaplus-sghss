57b9981f46a20ddb8d264a594a037b9b41ec38ae
Juliano <dti.juliano@fimca.com.br>
2025-03-27 14:37:12 -0400

    App 'pessoa': update models; Update admin for class Administrador; Update admin for class Pessoa

--- backend/pessoa/admin.py
+++ backend/pessoa/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import Pessoa, Administrador
 
-# Register your models here.
+
+class PessoaInline(admin.StackedInline):
+    model = Pessoa
+    can_delete = False
+    verbose_name_plural = 'Pessoa'
+    fk_name = 'usuario'
+    extra = 0
+    max_num = 1
+
+
+class CustomUserAdmin(DefaultUserAdmin):
+    inlines = (PessoaInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        return super().get_inline_instances(request, obj)
+    
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+@admin.register(Administrador)
+class AdministradorAdmin(admin.ModelAdmin):
+    list_display = ('idPessoa', 'cargo', 'idLocal')
+    search_fields = ('idPessoa__nome', 'idLocal__nome', 'cargo')
+    list_filter = ('cargo',)
--- backend/pessoa/models.py
+++ backend/pessoa/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from backend.local.models import Local
 
+
+CARGO = [
+    ('DIRGERAL', 'Diretor Geral'),
+    ('DIRFINAN', 'Diretor Financeiro'),
+    ('DIRADMIN', 'Diretor Administrativo'),
+    ('GESTOR', 'Gestor'),
+    ('LIDER', 'Lider'),
+]
 
 class Pessoa(models.Model):
     idPessoa = models.AutoField(
@@ -47,3 +56,32 @@ class Pessoa(models.Model):
 
     def __str__(self):
         return self.nome
+    
+
+class Administrador(models.Model):
+    idPessoa = models.ForeignKey(
+        Pessoa,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Pessoa'
+    )
+    idLocal = models.ForeignKey(
+        Local,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Local'
+    )
+    cargo = models.CharField(
+        max_length=50,
+        choices=CARGO,
+        blank=False,
+        verbose_name='Cargo'
+    )
+
+    def __str__(self):
+        return f"{self.idPessoa.nome} {self.cargo} de {self.idLocal.nome}"
+    
+    
+    class Meta:
+        verbose_name = 'Administrador'
+        verbose_name_plural = 'Administradores'
