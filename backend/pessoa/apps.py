31dc4318be358835ec7d657645653d9fc7223b2d
Juliano <dti.juliano@fimca.com.br>
2025-03-31 14:19:35 -0400

    Adjust apps file for 'pessoa' app, to include new models and admins

--- backend/pessoa/apps.py
+++ backend/pessoa/apps.py
@@ -1,7 +1,13 @@
-from django.apps import AppConfig
+# backend/pessoa/apps.py
 
+from django.apps import AppConfig
 
 class PessoaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.pessoa'
     verbose_name = 'Pessoa'
+
+    def ready(self):
+        from .admin import core_admin, saude_admin
+        if hasattr(core_admin, 'register_custom_user_admin'):
+            core_admin.register_custom_user_admin()
