df26421ab153c5ad0f34de692a19323b178ddddd
Juliano <dti.juliano@fimca.com.br>
2025-03-31 14:20:24 -0400

    Delete some files that has been moved or removed

--- backend/profissionalsaude/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProfissionalsaudeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend.profissionalsaude'
-    verbose_name = 'Profissional de Saude'
