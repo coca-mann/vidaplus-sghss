ae2d1987ea3d2a1afe3513db90eb19d91817b160
Juliano <dti.juliano@fimca.com.br>
2025-03-31 13:16:11 -0400

    Create admin registration on app 'profissionalsaude'

--- backend/profissionalsaude/admin.py
+++ backend/profissionalsaude/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import ProfissionalSaude, Especialidade, AgendaProfissionalSaude
 
-# Register your models here.
+@admin.register(ProfissionalSaude)
+class ProfissionalSaudeAdmin(admin.ModelAdmin):
+    list_display = (
+        'idProfissional',
+        'get_id_local',
+        'get_nome_pessoa',
+        'registroProfissional',
+        'get_especialidades'
+    )
+    search_fields = (
+        'idPessoa__nome',
+        'idEspecialidade__nome'
+    )
+    list_filter = ('especialidades',)
+
+    def get_id_local(self, obj):
+        return obj.idLocal.nome if obj.idLocal else '-'
+    get_id_local.short_description = 'Local'
+
+    def get_nome_pessoa(self, obj):
+        return obj.idPessoa.nome if obj.idPessoa else '-'
+    get_nome_pessoa.short_description = 'Nome'
+
+    def get_especialidades(self, obj):
+        return ", ".join([e.nome for e in obj.especialidades.all()])
+    get_especialidades.short_description = 'Especialidades'
+
+
+@admin.register(Especialidade)
+class ProfissionalEspecialidade(admin.ModelAdmin):
+    list_display = ('idEspecialidade', 'nome', 'descricao')
+    search_fields = ('nome',)
