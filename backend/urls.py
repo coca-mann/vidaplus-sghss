a1d5235a8f28655a680074ade2fb1c8564e9b610
Coca-Mann <juliano.ostroski@gmail.com>
2025-04-09 23:17:59 -0400

    feat(api): add root routes for atendimento, backoffice, local and pessoa

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('api/v1/', include('backend.atendimento.urls')),
+    path('api/v1/', include('backend.backoffice.urls')),
+    path('api/v1/', include('backend.local.urls')),
+    path('api/v1/', include('backend.pessoa.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
