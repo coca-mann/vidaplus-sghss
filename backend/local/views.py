61262a03f7baefac35c5340c86a37ede2433ca94
Coca-Mann <juliano.ostroski@gmail.com>
2025-04-09 22:56:57 -0400

    feat(local): implement API endpoints for Local

--- backend/local/views.py
+++ backend/local/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from backend.local.models import Local
+from backend.local.serializers import LocalSerializer
 
-# Create your views here.
+
+class LocalViewSet(ModelViewSet):
+    queryset = Local.objects.all()
+    serializer_class = LocalSerializer
+    permission_classes = [IsAuthenticated]
