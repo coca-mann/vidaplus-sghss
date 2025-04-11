8386e577246806834f283f7ebb12c2b731770881
Coca-Mann <juliano.ostroski@gmail.com>
2025-04-10 22:48:26 -0400

    feat(local): add endpoint for create and manage Local

--- backend/local/views.py
+++ backend/local/views.py
@@ -2,9 +2,10 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from backend.local.models import Local
 from backend.local.serializers import LocalSerializer
+from backend.local.permissions import IsAdminOrReadOnly
 
 
 class LocalViewSet(ModelViewSet):
     queryset = Local.objects.all()
     serializer_class = LocalSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminOrReadOnly]
