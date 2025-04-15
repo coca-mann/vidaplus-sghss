b08b1c23017e71300edac4857fb3e549fc989104
Coca-Mann <juliano.ostroski@gmail.com>
2025-04-14 21:19:20 -0400

    chore: remove all admins for django apps

--- backend/atendimento/admin/consulta.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from backend.atendimento.models.consulta import Consulta
-
-@admin.register(Consulta)
-class ConsultaAdmin(admin.ModelAdmin):
-    list_display = ('status',)
--- backend/pessoa/apps.py
+++ backend/pessoa/apps.py
@@ -6,8 +6,3 @@ class PessoaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.pessoa'
     verbose_name = 'Pessoa'
-
-    def ready(self):
-        from .admin import core_admin, saude_admin, paciente_admin
-        if hasattr(core_admin, 'register_custom_user_admin'):
-            core_admin.register_custom_user_admin()
