c0be9cc8bdfb778b91c0a82dfdc55022cb0dc29b
Coca-Mann <juliano.ostroski@gmail.com>
2025-04-22 22:46:01 -0400

    doc(gestaohospitalar): Added comments for ideas of endpoint validations

--- backend/backoffice/views/gestaohospitalar.py
+++ backend/backoffice/views/gestaohospitalar.py
@@ -11,19 +11,33 @@ from backend.backoffice.serializers.gestaohospitalar import (
     LogOcupacaoLeitoSerializer
 )
 
-
+'''
+ALA
+- Somente administradores podem editar alas
+- Médicos podem ver alas com leitos ocupados
+- Pacientes não podem ver alas
+'''
 class AlaViewSet(ModelViewSet):
     queryset = Ala.objects.all()
     serializer_class = AlaSerializer
     permission_classes = [IsAuthenticated]
 
-
+'''
+LEITOS
+- Somente administradores podem editar leitos
+- Médicos podem ver detalhes dos pacientes nos leitos
+'''
 class LeitoViewSet(ModelViewSet):
     queryset = Leito.objects.all()
     serializer_class = LeitoSerializer
     permission_classes = [IsAuthenticated]
 
-
+'''
+LOG LEITOS
+- Médicos podem internar pacientes, e liberar leitos
+- Administradores não podem internar nem liberar leitos
+- Pacientes podem ver apenas seus próprios leitos ocupado
+'''
 class LogOcupacaoLeitoViewSet(ModelViewSet):
     queryset = LogOcupacaoLeito.objects.all()
     serializer_class = LogOcupacaoLeitoSerializer
