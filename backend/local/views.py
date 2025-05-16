496717e0dd25cd1ac1ef33039c42acb665987645
Coca-Mann <juliano.ostroski@gmail.com>
2025-05-15 21:36:53 -0400

    fix(local): Removed some CRUD options from api docs

--- backend/local/views.py
+++ backend/local/views.py
@@ -16,26 +16,6 @@ from backend.local.permissions import IsAdminOrReadOnly
         description="Retorna os detalhes de um local específico pelo seu ID.",
         tags=["Locais"],
     ),
-    create=extend_schema(
-        summary="Criar local",
-        description="Permite criar um novo local. Apenas administradores podem realizar esta ação.",
-        tags=["Locais"],
-    ),
-    update=extend_schema(
-        summary="Atualizar local",
-        description="Atualiza todos os dados de um local existente. Apenas administradores podem realizar esta ação.",
-        tags=["Locais"],
-    ),
-    partial_update=extend_schema(
-        summary="Atualizar parcialmente local",
-        description="Atualiza parcialmente os dados de um local existente. Apenas administradores podem realizar esta ação.",
-        tags=["Locais"],
-    ),
-    destroy=extend_schema(
-        summary="Remover local",
-        description="Remove um local do sistema. Apenas administradores podem realizar esta ação.",
-        tags=["Locais"],
-    ),
 )
 class LocalViewSet(ModelViewSet):
     queryset = Local.objects.all()
